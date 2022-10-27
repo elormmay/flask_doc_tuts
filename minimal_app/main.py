2e9c2403f43392fc7d31545b88e0d8dee5176513
elormmay <elormmay@gmail.com>
2022-10-27 00:38:37 +0000

    Learned: Using templates and static files

--- minimal_app/main.py
+++ minimal_app/main.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from flask import Flask
 from markupsafe import escape
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello World</p>"
+
+
+@app.route("/sign_in")
+def sign_in():
+    return "Sign In"
 
 
 @app.route("/admin")
@@ -39,5 +45,51 @@ def about():
     return "The about page"
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# using url_for
+from flask import url_for
+
+with app.test_request_context():
+    print(url_for("hello_world"))
+    print(url_for("about"))
+    print(url_for("sign_in", next="/"))
+    print(url_for("projects"))
+
+
+# https methods
+
+from flask import request
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return "do_the_login()"
+    else:
+        return "show_the_login_form()"
+
+
+# or
+
+
+@app.get("/login")
+def login_get():
+    return "show_the_login_form()"
+
+
+@app.post("/login")
+def login_post():
+    return "do_the_login()"
+
+
+# Static files
+# url_for("static", filename="style.css") use in templates
+
+
+# Rendering templates
+from flask import render_template
+
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
